21e914ad4050a37ffc04efb642aa60f94df2f50e
Aleksey <nypko195@bk.ru>
2021-05-25 11:28:36 +0400

    add func getZero

--- js/main.js
+++ js/main.js
@@ -5,21 +5,33 @@ const timer = document.getElementById("only-time");
 const time = setInterval(function () {
    let date = new Date();
 
-   let seconds = date.getSeconds();
-   let minutes = date.getMinutes();
-   let hours = date.getHours();
+   let a = date.getSeconds();
+   let min = date.getMinutes();
+   let h = date.getHours();
 
+   let minutes = getZero(min);
+   let hours = getZero(h);
+   let seconds = getZero(a)
    
-   timer.innerHTML = `<span>${hours}</span>:<span>${minutes}</span>:<span>${seconds}</span>`;   
+   timer.innerHTML = `<span>${hours}</span><span>:</span><span>${minutes}</span><span>:</span><span>${seconds}</span>`;   
+   
 });
+
+function getZero(num) {
+   if(num >= 0 && num < 10) {
+      return `0${num}`;
+   } else {
+      return num;
+   }
+}
 
 //===weather===
 // API ключ
-let apiKey = "e281f29fffa4f3555a2a074c8e3d6581";
+const apiKey = "e281f29fffa4f3555a2a074c8e3d6581";
 // Город погода которого нужна
 let city = "Samara";
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?id=${city}&lang=ru&appid=${apiKey}`)
+fetch(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
 .then((resp) => {
    console.log(resp);
 })
