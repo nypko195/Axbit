630b715e8c59e8def60ba82a44fa643e2a9c3bc3
Aleksey <nypko195@bk.ru>
2021-05-31 06:14:41 +0400

    corrected names

--- js/modules/weather.js
+++ js/modules/weather.js
@@ -1,6 +1,6 @@
 const inputWeatherSity = document.querySelector('.weather__item-inp'),
       btnWeather = document.querySelector('.weather__item-btn'),
-      resultBd = document.querySelector('.weather__list');      
+      weatherListSelector = document.querySelector('.weather__list');      
 
 //загрузка из localStorage
 function getLocal() {
@@ -19,33 +19,16 @@ btnWeather.addEventListener('enter', sendForm)
 function sendForm(e) {
    e.preventDefault();
 
-   class InfoCity {
-      constructor(sity, degree, parentSelector) {
-         this.sity = sity;
-         this.degree = degree;
-         this.parent = document.querySelector(parentSelector);
-      }
-      renderNewWeatherCity() {
-         this.parent.insertAdjacentHTML('afterbegin', `
-            <div class="weather__result">
-               <p class="weather__result-city">${this.sity}</p>
-               <p class="weather__result-degree">${Math.round(this.degree)} C</p> 
-               <span class="weather__result-close">x</span>
-            </div>
-         `);
-      }
-   }
-
-   //==Ответ с БД==                     
+   //ответ с api                    
    let city = inputWeatherSity.value;
 
    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
       .then(resp => resp.json())
       .then((data) => {
 
-         const conditionMessageError = data.cod !== 200;
+         const isConditionMessageError = data.cod !== 200;
 
-         if (conditionMessageError) {
+         if (isConditionMessageError) {
             throw new Error(data.message);
          }
 
@@ -69,26 +52,44 @@ function sendForm(e) {
       })
 }
 
+class InfoCity {
+   constructor(sity, degree, parentSelector) {
+      this.sity = sity;
+      this.degree = degree;
+      this.parent = document.querySelector(parentSelector);
+   }
+   renderNewWeatherCity() {
+      this.parent.insertAdjacentHTML('afterbegin', `
+         <div class="weather__result">
+            <p class="weather__result-city">${this.sity}</p>
+            <p class="weather__result-degree">${Math.round(this.degree)} C</p> 
+            <span class="weather__result-close">x</span>
+         </div>
+      `);
+   }
+}
+
 //удаление погоды по клику
 function removeWeatherCity() {
    const selectedWeather = document.querySelectorAll('.weather__result');         
    selectedWeather.forEach(item => {
       item.addEventListener('click', e => {                
          item.remove(); 
-         removeWeatherLocalStorage(e.target.innerText)       
+         const clickedkWeather = e.target.innerText;
+         removeWeatherLocalStorage(clickedkWeather);       
       });      
    })
 }
 removeWeatherCity();
 
 //удаление из localStorage
-function removeWeatherLocalStorage(target) {             
-   localStorage.removeItem(target);   
+function removeWeatherLocalStorage(clickedWeatherCity) {             
+   localStorage.removeItem(clickedWeatherCity);   
 }
 
 //показ погоды из localStorage
 function showWeatherLocal(key, value) {
-   resultBd.insertAdjacentHTML('afterbegin', `
+   weatherListSelector.insertAdjacentHTML('afterbegin', `
    <div class="weather__result">
       <p class="weather__result-city">${key}</p>
       <p class="weather__result-degree">${Math.round(value)} C</p> 
